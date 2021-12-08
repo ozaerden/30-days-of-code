2d8cd7796a5630d38ba6ff9d24dcf0cfcc24cad6
Ozan Erden <ozaerden@hotmail.com>
2021-12-08 14:36:58 +0300

    Class name refactored

--- Day2.java
+++ Day2.java
@@ -9,13 +9,13 @@ public class Day2 {
         int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
         int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
 
-        Result.solve(meal_cost, tip_percent, tax_percent);
+        CalculateTotalCost.solve(meal_cost, tip_percent, tax_percent);
 
         bufferedReader.close();
     }
 }
 
-class Result{
+class CalculateTotalCost{
     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
         double total = meal_cost + (meal_cost*((double)tip_percent/100)) + (meal_cost*((double)tax_percent/100));
         System.out.println(Math.round(total));
